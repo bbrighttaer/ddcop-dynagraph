63dc17d1e97a31ba1f7aff6dc674eb8de78b74e5
Brighter <brighteragyemang@gmail.com>
2022-04-07 00:10:52 +1000

    enable metrics script to work on c-cocoa dcop metrics

--- mascoord/analysis/merge-sim-metric-files.py
+++ mascoord/analysis/merge-sim-metric-files.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 import tqdm
 
-keywords = ['-c-cocoa-', '-c-sdpop-', '-dcop-base-']
+keywords = ['-c-cocoa-', '-c-sdpop-', '-dcop-base-', 'c-cocoa-max-']
+metrics_dir = '../metrics/c-cocoa-dcop-metrics'
 
 for kwd in tqdm.tqdm(keywords):
-    data_files = [f for f in os.listdir('../metrics') if kwd in f and '.csv' in f]
+    data_files = [f for f in os.listdir(metrics_dir) if kwd in f and '.csv' in f]
 
     if data_files:
-        dataframes = [pd.read_csv(f'../metrics/{metrics_file}') for metrics_file in data_files]
+        dataframes = [pd.read_csv(f'{metrics_dir}/{metrics_file}') for metrics_file in data_files]
 
         df_merged = pd.DataFrame()
         df_merged['event'] = dataframes[0]['event']
