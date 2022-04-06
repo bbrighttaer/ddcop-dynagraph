6b45087f6330e842f25dd83db34a068e45fc804e
Brighter <brighteragyemang@gmail.com>
2022-04-06 14:32:59 +1000

    sim metric files merge script

---
+++ mascoord/analysis/merge-sim-metric-files.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+import numpy as np
+import tqdm
+
+keywords = ['-c-cocoa-', '-c-sdpop-', '-dcop-base-']
+
+for kwd in tqdm.tqdm(keywords):
+    data_files = [f for f in os.listdir('../metrics') if kwd in f and '.csv' in f]
+
+    if data_files:
+        dataframes = [pd.read_csv(f'../metrics/{metrics_file}') for metrics_file in data_files]
+
+        df_merged = pd.DataFrame()
+        df_merged['event'] = dataframes[0]['event']
+        df_merged['type'] = dataframes[0]['type']
+        df_merged['num_agents'] = dataframes[0]['num_agents']
+
+        data_columns = dataframes[0].columns[3:]
+        for col in data_columns:
+            col_data = [df[col] for df in dataframes]
+            df_merged[col] = np.mean(col_data, axis=0)
+
+        filename = data_files[0].split('.sim')[0] + kwd + 'merged-metrics.csv'
+        df_merged.to_csv(f'./{filename}', index=False)
