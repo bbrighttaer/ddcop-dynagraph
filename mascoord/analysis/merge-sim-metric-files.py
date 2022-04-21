3041d73f9e4d9006b9fe47ed6ac9dc5002a7baf3
Brighter <brighteragyemang@gmail.com>
2022-04-21 19:44:02 +1000

    experiments and analysis d3vd10

--- mascoord/analysis/merge-sim-metric-files.py
+++ mascoord/analysis/merge-sim-metric-files.py
@@ -4,10 +4,10 @@ import numpy as np
 import tqdm
 
 keywords = [
-    ('-c-cocoa-', 'max-deg-6', 'dc-cocoa'),
-    ('c-cocoa', 'max-deg-3', 'c-cocoa')
+    ('-c-cocoa-', 'max-deg-6', 'dc-cocoa-d10'),
+    # ('c-cocoa', 'max-deg-3', 'c-cocoa')
 ]
-metrics_dir = '../metrics/c-cocoa-dcop-metrics/'
+metrics_dir = '../metrics-collected-on-21-04-2022/'
 
 
 def main():
@@ -19,11 +19,16 @@ def main():
 
         if data_files:
             dataframes = [pd.read_csv(f'{metrics_dir}/{metrics_file}') for metrics_file in data_files]
-            print([df.shape for df in dataframes])
+            # print([df.shape for df in dataframes])
             df_merged = pd.DataFrame()
-            df_merged['event'] = dataframes[0]['event']
-            df_merged['type'] = dataframes[0]['type']
-            df_merged['num_agents'] = dataframes[0]['num_agents']
+
+            # select one df to copy invariable values
+            for df in dataframes:
+                if df.shape[0] == 120:
+                    df_merged['event'] = df['event']
+                    df_merged['type'] = df['type']
+                    df_merged['num_agents'] = df['num_agents']
+                    break
 
             data_columns = dataframes[0].columns[3:]
             for col in data_columns:
@@ -34,11 +39,13 @@ def main():
                     elif data_files[i] not in errored_files:
                         errored_files.append(data_files[i])
                 col = 'edge_' + col if col == 'cost' else col
+
                 df_merged[col] = np.mean(col_data, axis=0)
                 df_merged[col + '-std'] = np.std(col_data, axis=0)
 
             filename = f'{kwd[2]}-{kwd[1]}-merged-metrics.csv'
             df_merged.to_csv(f'./{filename}', index=False)
+
     print(files_count_dict)
     print(f'{len(errored_files)} errored files: {errored_files}')
 
